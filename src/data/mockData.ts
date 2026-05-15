1e7529d62a34619fdd097f357b2fe132e97f02ed
WP-Sync Bot (iPhone) <m4nu.r0ssi@gmail.com>
2026-05-15 15:01:26 +0000

    🔄 iPhone Sync: wp_full_sync - 2026-05-15 15:01

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -2,10 +2,10 @@ import { Recipe } from '../types';
 
 /**
  * Recettes synchronisées depuis WordPress — App iPhone
- * Dernière mise à jour: 15/05/2026 14:55:38
- * Total: 298 recettes
+ * Dernière mise à jour: 15/05/2026 15:01:26
+ * Total: 299 recettes
  */
-export const exportSyncId = "1778856938261";
+export const exportSyncId = "1778857286438";
 export const mockRecipes: Recipe[] = [
     {
         "id": "5624",
@@ -7252,6 +7252,39 @@ export const mockRecipes: Recipe[] = [
             "France",
             "Pas cher",
             "pâtisserie"
+        ],
+        "isFeatured": false,
+        "isFavorite": false,
+        "address": ""
+    },
+    {
+        "id": "5126",
+        "title": "Astuce pour préparer un poivron",
+        "description": "Découvrez une méthode simple et efficace pour couper et nettoyer un poivron rapidement, minimisant le gaspillage et le désordre.",
+        "image": "/api/image-proxy?url=http%3A%2F%2F109.221.250.122%2Fwordpress%2Fwp-content%2Fuploads%2F2026%2F04%2Fr-varietes-poivrons-couleurs-139254272as-662fdd6021b08.jpg&v=1776350302000",
+        "category": "plats",
+        "difficulty": "moyen",
+        "prepTime": 15,
+        "cookTime": 30,
+        "servings": 4,
+        "videoHtml": "<blockquote class=\"tiktok-embed\" cite=\"https://www.tiktok.com/v/6928474585725734149\" data-video-id=\"6928474585725734149\" style=\"max-width: 605px;min-width: 325px;\">\n<section>\n            <a target=\"_blank\" title=\"@tiktok\" href=\"https://www.tiktok.com/v/6928474585725734149\" rel=\"noopener noreferrer\">Regarder la vidéo sur TikTok</a><br />\n</section>\n</blockquote><script async src=\"https://www.tiktok.com/embed.js\"></script>",
+        "ingredients": [
+            {
+                "quantity": "",
+                "name": "🌶️\n 1 poivron"
+            }
+        ],
+        "steps": [
+            "Couper les deux extrémités du poivron.",
+            "Faire une incision verticale sur un côté du poivron.",
+            "Dérouler le poivron à plat et retirer facilement le cœur et les graines.",
+            "Couper le poivron en lanières ou en dés selon l&rsquo;utilisation souhaitée."
+        ],
+        "tags": [
+            "Astuces",
+            "Healthy",
+            "Pas cher",
+            "Végé"
         ],
         "isFeatured": false,
         "isFavorite": false,
